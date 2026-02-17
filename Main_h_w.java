5e7e0cd76734e4feffa3f1aa6f22f1ecb901098d
AtharvBhagat <159274676+AtharvBhagat@users.noreply.github.com>
2026-02-17 20:35:19 +0530

    Add files via upload

---
+++ Main_h_w.java
@@ -0,0 +1,12 @@
+package lesson_1;
+
+public class Main_h_w {
+    public static void main(String[] args){
+        int a=4;
+        int b=6;
+        
+
+        System.out.println("4+6= " + (a+b));
+       
+    }
+}
